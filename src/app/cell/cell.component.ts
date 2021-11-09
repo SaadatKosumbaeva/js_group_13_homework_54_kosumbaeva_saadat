f0e64ef8b7be882ab964a839d866e5cfe34f6100
Saadat Kosumbaeva <kosumbaevasaadat@gmail.com>
2021-11-09 11:09:29 +0600

    Implemented part of the game with the search for an item.

--- src/app/cell/cell.component.ts
+++ src/app/cell/cell.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input} from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 
 @Component({
   selector: 'app-cell',
@@ -7,9 +7,15 @@ import {Component, Input} from '@angular/core';
 })
 export class CellComponent {
   @Input() text = '';
+  @Output() cellClick = new EventEmitter();
   clicked = false;
 
   changeClassName() {
     return this.clicked ? 'cell white' : 'cell black';
+  }
+
+  onCellClick() {
+    this.clicked = !this.clicked;
+    this.cellClick.emit();
   }
 }
--- src/app/playing-field/playing-field.component.ts
+++ src/app/playing-field/playing-field.component.ts
@@ -6,14 +6,19 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./playing-field.component.css']
 })
 export class PlayingFieldComponent implements OnInit {
-  cells: { text: string }[] = [];
+  cells: {hasItem: boolean, text: string}[] = [];
+  randomIndex = Math.floor(Math.random() * 36);
 
   ngOnInit(): void {
     for (let i = 0; i < 36; i++) {
-      this.cells.push({text: ''});
+      this.cells.push({hasItem: false, text: ''});
     }
+  }
 
-    const randomIndex = Math.floor(Math.random() * this.cells.length);
-    this.cells[randomIndex].text = 'o';
+  pushText(index: number) {
+    if (index === this.randomIndex) {
+      this.cells[this.randomIndex].hasItem = true;
+      this.cells[this.randomIndex].text = 'o';
+    }
   }
 }
